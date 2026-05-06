05c3e721222eeb3462e980a009188d75468bf775
Chance Harmon <henrikillsit@gmail.com>
2026-05-06 15:11:02 -0600

    added color back to cubefaces

--- client/src/components/CubeFace.jsx
+++ client/src/components/CubeFace.jsx
@@ -1,13 +1,29 @@
 import './CubeFace.css'
 
+function getColor(value) {
+  const colorCode = value[0]
+  const map = {
+    w: 'white',
+    y: 'yellow',
+    g: 'green',
+    b: 'blue',
+    r: 'red',
+    o: 'orange',
+  };
+
+  return map[colorCode] || 'gray';
+}
+
 function CubeFace({ faceName, stickers }) {
+  console.log(faceName, stickers)
   return (
     <section>
       <h2>{faceName}</h2>
 
       <div className="cube-face">
         {stickers.map((sticker, index) => (
-          <div className={`sticker ${sticker}`} key={index}>
+          <div className="sticker"
+            style={{ backgroundColor: getColor(sticker) }} key={index}>
             {sticker}
           </div>
         ))}
